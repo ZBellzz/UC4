2f4fe6385f8a9ce90a3e597ff35b835ff461eab3
ZBellzz <petrusramos1@gmail.com>
2024-11-21 17:06:12 -0300

    21/11/2024

--- ATV5_EXERCICIO1.py
+++ ATV5_EXERCICIO1.py
@@ -2,15 +2,16 @@ lado1 = float(input('Informe o tamanho do lado 1: '))
 lado2 = float(input('Informe o tamanho do lado 2: '))
 lado3 = float(input('Informe o tamanho do lado 3: '))
 
-if lado1 + lado2 < lado3:
-   print('N é um triangulo')
-   
-elif lado1 == lado2 and lado1 == lado3 and lado2 == lado3:
-    print('é um triangulo equilatero')
-        
-elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
-    print('é um triangulo isosceles')
-     
-else:
-    print('é um escaleno')
+if lado1 + lado2 < lado3 and lado1 + lado3 < lado2:
 
+   
+    if lado1 == lado2 and lado1 == lado3 and lado2 == lado3:
+        print('é um triangulo equilatero')
+        
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        print('é um triangulo isosceles')
+     
+    else:
+        print('é um escaleno')
+else:
+    print('N é triangulo')
